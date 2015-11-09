1181a969239fc0abe92b9b40015d66b9757c6257
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-09 03:19:10 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,40 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+
+  def initialize(input)
+    @input = input
+  end
+
+  def input=(input)
+    @input = input
+  end
+
+     def input
+       @input
+     end
+
+    def lcm
+
+    divisor = @input    # @input
+    divisee = @input
+    mults = nil
+
+    while mults == nil
+ #      puts "ee #{divisee}"
+      divisor = @input
+      while divisor > 0
+        if divisee % divisor == 0
+   #          puts "#{divisee} / #{divisor}"
+           divisor -= 1
+           if divisor == 1
+              mults = divisee
+              puts mults
+           end
+        else
+          divisee += 1
+          divisor = 0
+        end
+      end
+    end
+    mults
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,26 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+
+  divisor = input
+  divisee = input
+  lcm = nil
+
+  while lcm == nil
+ #   puts "ee #{divisee}"
+#    divisee += 1
+    divisor = input
+    while divisor > 0
+      if divisee % divisor == 0
+     #     puts "#{divisee} / #{divisor}"
+         divisor -= 1
+         if divisor == 1
+            lcm = divisee
+            puts lcm
+         end
+      else
+        divisee += 1
+        divisor = 0
+      end
+    end
+  end
+  lcm
+end
